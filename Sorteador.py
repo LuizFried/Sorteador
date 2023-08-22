3e0c7570d16dee3d7c0902240bfdc42d3643d96b
LuizFried <luifried50@gmail.com>
2023-08-21 23:01:25 -0300

    v2

--- Sorteador.py
+++ Sorteador.py
@@ -9,44 +9,39 @@ janela.geometry('700x400')
 janela.resizable(True, True)
 
 lista = list()
+
 def sortear(x):
+
     if x.isnumeric():
         rep = int(x)
 
-    try:
-        for i in range (0, rep):
-            lista.append(i)
-    except (KeyboardInterrupt, ValueError):
-        messagebox.showerror('ERRO', 'FALHA AO REGISTRAR NÚMEROS')
+        try:
+            for i in range(0, rep):
+                lista.append(i)
+        except (KeyboardInterrupt, ValueError):
+            messagebox.showerror('ERRO', 'FALHA AO REGISTRAR NÚMEROS')
 
-    numb = randint(0,len(lista))
+        numb = randint(1, len(lista))
 
-    sorteado = ctk.CTkLabel(resp, width=15,text=f'{numb}',
-                   font=('Arial', 30, 'bold'))
+        resp.configure(text=f'{numb}')
+        lista.clear()
 
-    sorteado.pack(padx=3, pady=5)
+    else:
+        messagebox.showerror('ERRO', 'DIGITE UMA VALOR VÁLIDO')
 
 
 texto = ctk.CTkLabel(janela, text='Quantidade de números', text_color='RED')
-texto.pack()
+texto.pack(padx=10, pady=10)
+
 numb_rep = ctk.CTkEntry(janela, width=50)
-numb_rep.pack()
-button = ctk.CTkButton(janela, text='Sortear', command=lambda: sortear(numb_rep.get()), hover_color='green',fg_color='black')
-button.pack(padx=10,pady=10)
-resp =ctk.CTkFrame(janela)
-resp.pack()
+numb_rep.pack(padx=10, pady=10)
 
+button = ctk.CTkButton(janela, text='Sortear', command=lambda: sortear(numb_rep.get()), hover_color='green',
+                       fg_color='black')
+button.pack(padx=10, pady=10)
 
-
-
-
-
-
-
-
-
-
-
+resp = ctk.CTkLabel(janela, text=' ', text_color='red', width=50, font=('arial', 35))
+resp.pack(padx=10, pady=10)
 
 
 janela.mainloop()
